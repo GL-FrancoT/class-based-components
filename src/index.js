52b6e10d4047239ee1da74ccdf5a0eb5159d5d79
Franco Traverso <franco.traverso@globallogic.com>
2022-07-26 15:40:46 -0300

    Implement geolocation api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 const App = () => {
-    return <div>Hi there!</div>;
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  );
+  return <div>Hi there!</div>;
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector("#root"));
